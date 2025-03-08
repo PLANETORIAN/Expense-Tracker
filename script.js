dfe5c21cfbaac492867d3336e7c718304e0b6f07
PLANETORIAN <daksh06shah2006@gmail.com>
2025-03-08 14:36:49 +0530

    adding debit and credit

--- script.js
+++ script.js
@@ -109,14 +109,14 @@ function nameClick(selectedName){
                             <p>category
                             </p>
                             <div class="cat-container">
-                                <div class="cats"></div>
-                            <div class="cats"></div>
-                            <div class="cats"></div>
-                            <div class="cats"></div>
-                            <div class="cats"></div>
-                            <div class="cats"></div>
-                            <div class="cats"></div>
-                            <div class="cats"></div>
+                                <div class="cats">0</div>
+                            <div class="cats">1</div>
+                            <div class="cats">2</div>
+                            <div class="cats">3</div>
+                            <div class="cats">4</div>
+                            <div class="cats">5</div>
+                            <div class="cats">6</div>
+                            <div class="cats">7</div>
                             </div>
                             
                         </div>
@@ -130,9 +130,79 @@ function nameClick(selectedName){
                 
         </div>
     </div>`
+
+    let selCat=0;
+    document.querySelectorAll(".cats").forEach((cat)=>{
+        cat.addEventListener("click", function (){
+            selCat = this.innerText;
+
+            //chatgpted
+
+            document.querySelectorAll(".cats").forEach(c => c.style.border = "none");
+        
+            this.style.border = "2px solid red";
+        }   
+        )
+    })
+
+    let addDeposit = content.querySelector(".add-balance");
+    addDeposit.addEventListener("click", ()=> addCredit(selectedName));
+
+    let addExpense=content.querySelector(".add-expense");
+    
+    addExpense.addEventListener("click",()=>{addDebit(selectedName,selCat)
+        document.querySelectorAll(".cats").forEach(c => c.style.border = "none")
+    });
+
 }
 
-function addDebit(){
+
+function addDebit(selectedName,selCat){
+    let debInput=document.querySelector(".debit");
+    let newDeb = debInput.value;
+    let debNote=document.querySelector(".d-text");
+    let newDNote= debNote.value;
     
+    
+    if (newDeb != ""){
+
+        let userIndex = users.findIndex(user => user.name === selectedName);
+
+
+        //chatgpted 
+
+
+        if (!users[userIndex].debit) {
+            users[userIndex].debit = [];
+        }
+        users[userIndex].debit.push({ balance: newDeb, note: newDNote, category:selCat });
+
+        localStorage.setItem("users", JSON.stringify(users));
+    debInput.value = "";
+    debNote.value = "";
+
+    
+    }
 }
+
+function addCredit(selectedName){
+    let creInput=document.querySelector(".credit");
+    let newCre = creInput.value;
+    let creNote = document.querySelector(".c-text");
+    let newCNote = creNote.value;
+
+    if (newCre!=""){
+        let userIndex = users.findIndex(user => user.name === selectedName);
+
+        if (!users[userIndex].credit){
+            users[userIndex].credit = [];
+
+        }
+        users[userIndex].credit.push({balance: newCre, note: newCNote});
+        localStorage.setItem("users",JSON.stringify(users));
+    creInput.value="";
+    creNote.value="";
+    }
+}
+
 
